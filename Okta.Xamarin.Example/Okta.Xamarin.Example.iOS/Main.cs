215f0a4dfe574258961bc45fd05e3552d188d5c0
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-10 09:15:11 -0800

    make generic for templates

--- Okta.Xamarin.Example/Okta.Xamarin.Example.iOS/Main.cs
+++ Okta.Xamarin.Example/Okta.Xamarin.Example.iOS/Main.cs
@@ -12,9 +12,9 @@ namespace Okta.Xamarin.Example.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
+            // if you want to use a different Application Delegate class from "MyAppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, "MyAppDelegate");
         }
     }
 }
